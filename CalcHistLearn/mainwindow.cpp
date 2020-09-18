9caddb31eb0a0ac710acc633622d7f318b8545b1
liang.xie <liang.xie>
2020-09-18 14:23:18 +0800

    增加對彩色圖像求直方圖的代碼，暫時沒解決

--- CalcHistLearn/mainwindow.cpp
+++ CalcHistLearn/mainwindow.cpp
@@ -97,27 +97,30 @@ void MainWindow::on_pushButton_3_clicked()
         return ;
     }
 
-    cv::Mat hsv;
-    cvtColor(src, hsv, COLOR_BGR2HSV);
      // Quantize the hue to 30 levels
      // and the saturation to 32 levels
-     int hbins = 30;
-     int sbins = 32;
-     int histSize[] = {hbins, sbins};
-     // hue varies from 0 to 179, see cvtColor
-     float hranges[] = { 0, 180 };
-     // saturation varies from 0 (black-gray-white) to
-     // 255 (pure spectrum color)
-     float sranges[] = { 0, 256 };
-     const float* ranges[] = { hranges, sranges };
+     int rBins = 256;
+     int gBins = 256;
+     int bBins = 256;
+
+     int histSize[] = {rBins, gBins, bBins};
+
+     // r,g,b varies from 0 to 255, see cvtColor
+     float rRanges[] = { 0, 256 };
+     float gRanges[] = { 0, 256 };
+     float bRanges[] = { 0, 256 };
+
+     const float* ranges[] = { rRanges, gRanges, bRanges };
      MatND hist;
-     // we compute the histogram from the 0-th and 1-st channels
-     int channels[] = {0, 1};
-     calcHist( &hsv, 1, channels, Mat(), hist, 2, histSize, ranges, true, false );
+     // we compute the histogram from the 0-th and 2-st channels
+     int channels[] = {0, 1, 2};
+     calcHist( &src, 1, channels, Mat(), hist, 3, histSize, ranges, true, false );
      qDebug() << "hist type" << hist.type() << endl;
 
      ofstream fout;
      fout.open("pixel.txt", ios::app);
+     qDebug() << "histRows" << hist.rows << endl;
+     qDebug() << "histCols" << hist.cols << endl;
      for(int i = 0; i < hist.rows; i++) {
          for(int j = 0; j < hist.cols; j++) {
              fout << hist.at<float>(i, j) << " ";
@@ -125,22 +128,22 @@ void MainWindow::on_pushButton_3_clicked()
          fout << endl;
      }
 
-     double maxVal = 0;
-     minMaxLoc(hist, 0, &maxVal, 0, 0);
-     int scale = 10;
-     Mat histImg = Mat::zeros(sbins*scale, hbins*10, CV_8UC3);
-     for( int h = 0; h < hbins; h++ )
-         for( int s = 0; s < sbins; s++ )
-         {
-             float binVal = hist.at<float>(h, s);
-             int intensity = cvRound(binVal*255/maxVal);
-             rectangle( histImg, Point(h*scale, s*scale), Point( (h+1)*scale - 1, (s+1)*scale - 1),
-                        Scalar::all(intensity),
-                        CV_FILLED );
-         }
-     namedWindow( "Source", 1 );
-     imshow( "Source", src );
-     namedWindow( "H-S Histogram", 1 );
-     imshow( "H-S Histogram", histImg );
+//     double maxVal = 0;
+//     minMaxLoc(hist, 0, &maxVal, 0, 0);
+//     int scale = 10;
+//     Mat histImg = Mat::zeros(sbins*scale, hbins*10, CV_8UC3);
+//     for( int h = 0; h < hbins; h++ )
+//         for( int s = 0; s < sbins; s++ )
+//         {
+//             float binVal = hist.at<float>(h, s);
+//             int intensity = cvRound(binVal*255/maxVal);
+//             rectangle( histImg, Point(h*scale, s*scale), Point( (h+1)*scale - 1, (s+1)*scale - 1),
+//                        Scalar::all(intensity),
+//                        CV_FILLED );
+//         }
+//     namedWindow( "Source", 1 );
+//     imshow( "Source", src );
+//     namedWindow( "H-S Histogram", 1 );
+//     imshow( "H-S Histogram", histImg );
      waitKey();
 }
